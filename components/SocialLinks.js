10a9afe2e68fdfea1d880e1c300b4074a17457c3
Joe Astuccio <joe@astucc.io>
2021-03-10 08:57:51 -0500

    Remove unused PUXL icon

--- components/SocialLinks.js
+++ components/SocialLinks.js
@@ -1,7 +1,5 @@
 import FreeCodeCamp from './Icons/FreeCodeCamp';
 
-import { IconAccessibility } from 'react-puxl-icons';
-
 export const App = () => (
   <div>
     <FreeCodeCamp width="3em" height="3em" fill="red" />
